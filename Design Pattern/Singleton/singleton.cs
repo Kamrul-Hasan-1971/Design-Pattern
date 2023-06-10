0572c744132da673ceffb932b02d5238314a5975
Md. Kamrul Hasan <icthasan36@gmail.com>
2023-06-10 13:54:39 +0600

    Add sigletone pattern details.

--- Design Pattern/Singleton/singleton.cs
+++ Design Pattern/Singleton/singleton.cs
@@ -6,6 +6,21 @@ using System.Threading.Tasks;
 
 namespace Design_Pattern.Singleton
 {
+    /*
+    Definition:
+        Singleton is a creational design pattern that lets you ensure that a class has only one instance, while providing a global access point to this instance.
+    Problem:
+        The Singleton pattern solves two problems at the same time, violating the Single Responsibility Principle:
+        1) Ensure that a class has just a single instance.
+        2) Provide a global access point to that instance.
+    Pros: 
+        1) You can be sure that a class has only a single instance.
+        2) You gain a global access point to that instance.
+        3) The singleton object is initialized only when it’s requested for the first time.
+    Cons: 
+        1) Violates the Single Responsibility Principle. The pattern solves two problems at the time.
+        2) The pattern requires special treatment in a multithreaded environment so that multiple threads won’t create a singleton object several times.
+    */
     public sealed class Singleton
     {
         private Singleton() { }
